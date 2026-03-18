8cd9fe57a2438745e5146abbb8ef7e03ec3a8f17
DFelipehDEV <thediogopcm@gmail.com>
2026-03-18 10:18:35 +0000

    add avalable field to space model

--- backend/models/Space.js
+++ backend/models/Space.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const spaceSchema = new mongoose.Schema({
+    available: Boolean,
     description: String,
     capacity: Number,
     pricePerHour: Number,
