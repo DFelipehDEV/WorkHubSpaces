dc08a5cb22ac3fa57e0c237114ad154d36f51e18
DFelipehDEV <thediogopcm@gmail.com>
2026-03-18 11:26:04 +0000

    add login route

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const bcrypt = require('bcryptjs');
 router.post('/users', urlencodedParser, async (req, res) => {
     try {
         await User.create({
-            role: req.body.role,
+            role: "69aaab200457e0ba9d55fd1d",
             name: req.body.name,
             password: await bcrypt.hash(req.body.password, 8),
             email: req.body.email,
@@ -26,10 +26,31 @@ router.post('/users', urlencodedParser, async (req, res) => {
             nif: req.body.nif,
             activity: req.body.activity,
             company: req.body.company,
-            suspended: req.body.suspended,
+            suspended: false,
         });
         //TODO: criar e returnar jwt
         res.status(201).send("User created");
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+router.post('/login', urlencodedParser, async (req, res) => {
+    try {
+        const user = await User.findOne({
+            email: req.body.email,
+        }).orFail().exec();
+        if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+            return res.status(500).send("User doesn't exist or the passord or/and the email are wrong")
+        }
+        if (user.suspended) {
+            return res.status(500).send("User is suspended");
+        }
+        //TODO: enviar token jwt
+        res.status(200).json({
+            message: "Success",
+            name: user.name,
+        });
     } catch (err) {
         res.status(500).send(err.message);
     }
