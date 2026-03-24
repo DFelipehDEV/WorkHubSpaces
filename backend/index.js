4a590e71489e4cb9351d65c00422c4ad268b924c
Zasus <thediogopcm@gmail.com>
2026-03-24 16:33:27 +0000

    remove bodyparser dependency

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
-const urlencodedParser = bodyParser.urlencoded();
 
 mongoose.connect(process.env.ATLAS_CONNECTION);
 
@@ -11,16 +9,16 @@ const UserController = require('./controllers/UserController');
 app.get('/users/:id', UserController.getId);
 
 const AuthController = require('./controllers/AuthController');
-app.post('/signin', urlencodedParser, AuthController.signIn);
-app.post('/login', urlencodedParser, AuthController.login);
+app.post('/signin', express.urlencoded(), AuthController.signIn);
+app.post('/login', express.urlencoded(), AuthController.login);
 
 const ReservationController = require('./controllers/ReservationController');
 app.get('/reservations/:id', ReservationController.getId);
-app.post('/reservations', urlencodedParser, ReservationController.create);
+app.post('/reservations', express.urlencoded(), ReservationController.create);
 
 const SpaceController = require("./controllers/SpaceController");
-app.post('/spaces', urlencodedParser, SpaceController.create);
+app.post('/spaces', express.urlencoded(), SpaceController.create);
 
 app.listen(process.env.PORT, () => {
-  console.log(`http://localhost:${process.env.PORT}`)
+    console.log(`http://localhost:${process.env.PORT}`)
 });
