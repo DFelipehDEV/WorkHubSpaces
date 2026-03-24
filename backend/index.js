41937777972362cc4749dd45aab32af45d25bbea
Zasus <thediogopcm@gmail.com>
2026-03-24 16:43:48 +0000

    move to json bodies

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,25 @@ const express = require('express');
 
 const app = express();
 
-mongoose.connect(process.env.ATLAS_CONNECTION);
-
 const UserController = require('./controllers/UserController');
 app.get('/users/:id', UserController.getId);
 
 const AuthController = require('./controllers/AuthController');
-app.post('/signin', express.urlencoded(), AuthController.signIn);
-app.post('/login', express.urlencoded(), AuthController.login);
+app.post('/signin', express.json(), AuthController.signIn);
+app.post('/login', express.json(), AuthController.login);
 
 const ReservationController = require('./controllers/ReservationController');
 app.get('/reservations/:id', ReservationController.getId);
-app.post('/reservations', express.urlencoded(), ReservationController.create);
+app.post('/reservations', express.json(), ReservationController.create);
 
 const SpaceController = require("./controllers/SpaceController");
-app.post('/spaces', express.urlencoded(), SpaceController.create);
+app.post('/spaces', express.json(), SpaceController.create);
 
 app.listen(process.env.PORT, () => {
-    console.log(`http://localhost:${process.env.PORT}`)
+    try {
+        mongoose.connect(process.env.ATLAS_CONNECTION);
+    } catch (ex) {
+        console.log(ex.message);
+    }
+    console.log(`http://localhost:${process.env.PORT}`);
 });
