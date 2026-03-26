3e7f6247b6572b7624f7379906ec05bdd9cdfe70
DFelipehDEV <thediogopcm@gmail.com>
2026-03-26 11:24:14 +0000

    make ExtraService fields required

--- backend/models/ExtraService.js
+++ backend/models/ExtraService.js
@@ -2,10 +2,19 @@ const mongoose = require("mongoose");
 
 const extraServiceSchema = new mongoose.Schema({
     // type: Number, // Impressoes/Coffee Break/Equipamento audiovisual/cacifo temporario
-    name: String,
+    name: {
+        type: String,
+        required: true,
+    },
     description: String,
-    price: Number,
-    available: Boolean,
+    price: {
+        type: Number,
+        required: true,
+    },
+    available: {
+        type: Boolean,
+        required: true,
+    },
 });
 
 module.exports = mongoose.model('ExtraService', extraServiceSchema);
