38c838e34f19b38adf3f2632380b2a10a73ebc90
DFelipehDEV <thediogopcm@gmail.com>
2026-03-26 10:04:27 +0000

    add space type and make available and capacity required

--- backend/models/Space.js
+++ backend/models/Space.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
 
 const spaceSchema = new mongoose.Schema({
-    available: Boolean,
+    type: {
+        type: String,
+        required: true,
+    },
+    available: {
+        type: Boolean,
+        required: true,
+    },
     description: String,
-    capacity: Number,
+    capacity: {
+        type: Number,
+        required: true,
+    },
     pricePerHour: Number,
-    images: [String]
+    images: [String],
+    // reviews
 });
 
 module.exports = mongoose.model('Space', spaceSchema);
