9465c5db7a7864c338daa717ed0be9efbadcea26
DFelipehDEV <thediogopcm@gmail.com>
2026-03-26 11:47:44 +0000

    add error handling in Get User route

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,11 +1,14 @@
 const User = require("../models/User");
 
 exports.getId = async (req, res) => {
-    res.send(await User.findById(req.params.id).select({
+    const user = await User.findById(req.params.id).orFail(() => {
+        return res.status(404).send({ message: "Couldn't find user" });
+    }).select({
         password: false
-    }).exec());
-};
+    });
 
+    return res.status(200).send(user);
+};
 
 exports.getAll = async (req, res) => {
     res.send(await User.find({}));
